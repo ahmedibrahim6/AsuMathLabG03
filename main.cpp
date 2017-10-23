8461585a365bc895d0b1b0d8ee77d62c6e9e1ca5
ZIZO96 <31322921+ZIZO96@users.noreply.github.com>
2017-10-23 23:39:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	
 	CMatrix A(3, 3, 2.0, 4.0, 4.0, 2.0, 2.0, 5.0, 6.0, 4.0, 4.0), B(3.0, 3.0, 1.0, 1.0, 2.0, 2.0, 3.0, 4.0, 2.0, 4.0, 3.0), C=1.5;
-
+	CMatrix D(3, 2, 1.0, 2.0, 1.0, 2.0, 1.0, 2.0), E(3, 1, 5.0, 6.0, 2.0);
 	try {
 		C = A - C;
 	}
@@ -15,8 +15,19 @@ int main()
 	{
 		cout << error << endl;
 	}
+	try {
+		D = D*E;
+	}
+	catch(char* error)
+	{
+		cout << error << endl;
+	}
 	C.PrintMatrix();
-	/*A = B + A;
-	A.PrintMatrix();*/
+	A = B + A;
+	A.PrintMatrix();
+	D.PrintMatrix();
+	D=A*D;
+	D.PrintMatrix();
+
 
 }
