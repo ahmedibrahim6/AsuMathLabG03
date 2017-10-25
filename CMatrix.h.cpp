fecfd8415e748bbc25a9e72690650a4160b840d3
ahmedibrahim6 <aibrahim66@yahoo.com>
2017-10-26 00:49:35 +0200

    Update CMatrix.h.cpp

--- CMatrix.h.cpp
+++ CMatrix.h.cpp
@@ -10,29 +10,29 @@ public:
 		   enum MI { MI_ZEROS, MI_ONES, MI_EYE, MI_RAND, MI_VALUE };
 		   CMatrix(int nR, int nC, int initialization = MI_ZEROS, double initializationValue = 0.0);
 		   CMatrix(int nR, int nC, double first, ...);
-		   CMatrix(CMatrix& m);
+		   CMatrix(CMatrix const& m);
 		   CMatrix(double d);
 		  // CMatrix(string s);
-		   void copy(CMatrix& m);
+		   void copy(CMatrix const& m);
 		   void copy(double d);
 		   //void copy(string s);
 		   void reset();
 		   //string getString();
-		   CMatrix operator=(CMatrix& m);
+		   CMatrix operator=(CMatrix const& m);
 		   CMatrix operator=(double d);
 		   //CMatrix operator=(string s);
 
 
 
-		   void add(CMatrix& m);
-		   void operator+=(CMatrix& m);
+		   void add(CMatrix const& m);
+		   void operator+=(CMatrix const& m);
 		   void operator+=(double d);
-		   CMatrix operator+(CMatrix& m);
+		   CMatrix operator+(CMatrix const& m);
 		   CMatrix operator+(double d);
-		   void sub(CMatrix& m);
-		   void operator-=(CMatrix& m);
+		   void sub(CMatrix const& m);
+		   void operator-=(CMatrix const& m);
 		   void operator-=(double d);
-		   CMatrix operator-(CMatrix& m);
+		   CMatrix operator-(CMatrix const& m);
 		   CMatrix operator-(double d);
 		   CMatrix operator++();
 		   CMatrix operator++(int);
@@ -41,18 +41,33 @@ public:
 		   CMatrix operator-();
 		   CMatrix operator+();
 	
+	
+	
+	           void mul(CMatrix const& m);
+	           void operator*=(CMatrix const& m);
+	           void operator*=(double d);
+	           CMatrix operator*(CMatrix const& m);
+	           CMatrix operator*(double d);
+	
+		   
+	
 		   double getDeterminant();
 		   CMatrix getCofactor(int r, int c);
 		   CMatrix adjoint();
 		   CMatrix inverse();
-		   void div(CMatrix& m);
-		   CMatrix operator/(CMatrix& m);
-		   void operator/=(CMatrix& m);
+	
+	
+	
+		   void div(CMatrix const& m);
+		   CMatrix operator/(CMatrix const& m);
+		   void operator/=(CMatrix const& m);
+	
+	
 
 		   void PrintMatrix();
 
 
-		   void mul(CMatrix& m); void operator*=(CMatrix& m); void operator*=(double d); CMatrix operator*(CMatrix& m);CMatrix operator*(double d);
+		   
 	
 };
 
