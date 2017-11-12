9cb4502564b0be7c45935dc1c64a9ce99aaa04d2
ahmedibrahim6 <aibrahim66@yahoo.com>
2017-11-12 14:49:30 +0200

    update main to receive the input file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,59 @@
 
 #include "CMatrix.h"
-int main()
+#include <fstream>
+
+
+
+int main(int argv,char* argc[]) // argv   :   number of strings separated by spaces in command line . 
+				// argc[] :   content of each string . 
+				// argv will be the number of strings pointed to by argc.
+				// This will (in practice) be 1 plus the number of arguments,
+				// as virtually all implementations will prepend the name of the program to the array.
+				
+	
 {
 	
-	CMatrix A(3, 3, 2.0, 4.0, 4.0, 2.0, 2.0, 5.0, 6.0, 4.0, 4.0), B(3, 3, 1.0, 1.0, 2.0, 2.0, 3.0, 4.0, 2.0, 4.0, 3.0), C=1.5;
+	 if(argv>1)
+		 
+        {
+		 
+                cout<<argc[1]<<endl;
+                 
+
+                string s;
+                ifstream infile(argc[1]);
+                
+                while(getline(infile, s))
+				{
+					cout<<s<<endl;
+
+
+
+
+				// we now have the first line in the input file
+				// we should discover the instruction that the line points to . then, send it to the functions 
+				//of the CMatrix class to make our calculations.
+
+
+
+					
+
+
+				}
+
+
+                infile.close();
+                cout<<endl;
+        }
+
+
+        return 0;
+}
+/*
+	CMatrix A=4;
+	A = " [ 2.0 4.0 4.0 ;2.0 2.0 5.0 ; 6.0 4.0 4.0 ]"  ;
+	
+	CMatrix B(3, 3, 1.0, 1.0, 2.0, 2.0, 3.0, 4.0, 2.0, 4.0, 3.0), C=1.5;
 	CMatrix D(3, 2, 1.0, 2.0, 1.0, 2.0, 1.0, 2.0), E(3, 1, 5.0, 6.0, 2.0);
 	try {
 		C = A - C;
@@ -43,4 +93,7 @@ int main()
 	getline(cin,x);
 	gg=x;
 	gg.PrintMatrix();
+	string y;
+	y=result.getString();
+	cout<<y<<endl;
 }
