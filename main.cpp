2447d554e41f8e8afe72604a1b3dd4cf4827200d
Ahmed M.Ibrahim <aibrahim66@yahoo.com>
2018-01-24 12:26:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 
 #include "CMatrix.h"
 
+void string_parsing(string s)
 
+{
+	char* buffer = new char[s.length()+1]; 
+strncpy(buffer,s.c_str(),s.length()+1);
+	//cout<<buffer<<endl;
+	delete[] buffer;
 
+}
+
+bool is_one_line(string s)
+{
+	int open_bracket_count=0;
+	int close_bracket_count=0;
+	for(int i=0;i<s.length();i++)
+	{
+		if(s[i]=='[')
+			open_bracket_count++;
+
+	}	
+	for(int i=0;i<s.length();i++)
+	{
+		if(s[i]==']')
+			close_bracket_count++;
+
+	}	
+	cout<<open_bracket_count<<close_bracket_count<<endl;
+	if(close_bracket_count==open_bracket_count)
+		return true;
+	else
+		return false;
+
+}
 
 
 
@@ -23,10 +54,21 @@ if(argv>1)			// ensure that the file path added
 	
 		while (getline(infile,s1))                       //  read every line in string s1
 		{
+			//while(s1=="")	getline(infile,s1);
+			
+			
 			if(s1.find('[')!=-1 )                   // if this line contains matrix declaration 
 			{
-				if(s1.find(']')!=-1)            // if the declaration in one line only we work on s1
+				//int brackets[2];
+				bool one_line = is_one_line(s1);
+
+
+				if(one_line)            // if the declaration in one line only we work on s1
 				{
+					//string_parsing(s1);
+
+
+
 
 					Matrix_String=s1.substr(s1.find('['),s1.length()-s1.find('['));   // have the string from '[' to ']'
 
@@ -44,11 +86,17 @@ if(argv>1)			// ensure that the file path added
 			 			}
 					}
 				}
-				else                      //  if the declaration in more than one line 
+				else                     //  if the declaration in more than one line 
 				{
 			
-					getline(infile,s2,']');          // we get the second line until ']'. store it in s2 
+					getline(infile,s2,']');
+					//cout<<s1<<endl<<s2<<endl;          // we get the second line until ']'. store it in s2 
 					s2=s1+s2+']';			//and add it to the first line (s1)
+					cout<<s2<<endl;
+					//string_parsing(s2);
+
+
+
 					Matrix_String=s2.substr(s2.find('['),s2.length()-s2.find('['));
 					
 					for(int i=0;i<s2.length(); i++)                   
@@ -71,6 +119,7 @@ if(argv>1)			// ensure that the file path added
 		
 
 				}
+				
 
 			}
 			
@@ -143,7 +192,7 @@ if(argv>1)			// ensure that the file path added
 						}
 					}	
 
-					                                        if(int x=s1.find("ones")!=-1)
+					if(int x=s1.find("ones")!=-1)
 					{	
 						int first=s1.find("(",x+1);
                                                 int mid =s1.find(",",first+1) ;
@@ -186,7 +235,7 @@ if(argv>1)			// ensure that the file path added
 					}
 
 
-                                        if(int x=s1.find("eye")!=-1)
+                    if(int x=s1.find("eye")!=-1)
 					{	
 						int first=s1.find("(",x+1);
                                                 int mid=s1.find(",",first+1);
@@ -374,10 +423,38 @@ if(argv>1)			// ensure that the file path added
 					}
 		  
 				}
+			
 
+			}
+
+
+
+			else if ((s1.length()==2))//||(s1.length()==1))
+			{
+				map<char,CMatrix>::iterator it1;
+				it1 =mymap.find(s1[0]);
+				cout<<(it1->second)<<endl;
+				
+				/*map<char,CMatrix>::iterator it1;
+				int flag =0;
+				for(int i=0;i<s1.length(); i++)                   
+		    		{
+						if(s1[i]==' '||s2[i]=='\n'||s2[i]=='\r')             // we skip spaces and newlines
+		    				
+							continue;
+
+						else
+						{
+							it1 =mymap.find(s1[i]);
+							cout<<(it1->second)<<endl;
+							break;
+						}
+					}
+*/
 			}
 		
 		}
+	
 
 	 infile.close();             // close the file 
 
@@ -407,306 +484,3 @@ return 0;
 
 
 
-
-
-
-
-
-
-
-
-
-
-/*
-		getline(infile,s,'[');
-		s+=']';
-		//Matrix_String=stringtrim(s);
-		Matrix_String=s.substr(s.find('['),s.length()-s.find('['));              // get matrix string as [....;....;...]
-		//cout<<Matrix_String<<endl;
-		Mnames[count]=Matrix_String;               //store matrix in the array
-
-		for(int i=0;i<s.length(); i++)                    // to store the names
-		{
-			if(s[i]==' '||s[i]=='\n'||s[i]=='\r')                  // we skip spaces and newlines
-				continue;
-			else
-			{
-				names[count]=s[i];                   // and store the first char ie. A or B
-				break;
-			}
-
-				
-		}
-		count++;                                      // increment count to signal a matrix is read
-		getline(infile,s);                           // extra getline to remove trailing newlines
-	
-
-	}
-
-	while ( getline(infile,s))                         
-	{
-		
-
-	}
-	
-*/
-/*
-	for (int i=0; i<counter; i++)
-	{
-		names[count]=operations[i][0];                    //store the first elements in operation in the names array ie. CDEFGH
-		count++;
-	}
-
-
-	/////////////////////////////////////////////
-	
-	cout<<endl<<names[0]<<'='<<endl;
-	cout<<Mnames[0].getString()<<endl<<endl; 
-	
-	cout<<names[1]<<'='<<endl;
-	
-	cout<<Mnames[1].getString()<<endl<<endl;
-	
-	
-	
-	////////////////////////////////////////
-
-	/*for (int i=0;i<count;i++)           //  to print the array of names 
-		cout<<names[i];
-	*/	
-//try{
-/*
-	for (int i=0; i<12;i++)                    // for loop to pass on array of operations
-	{
-		for (int j=0;j<6; j++)
-		{
-			if (operations[i][j]=='+')
-			{
-				for(int x=0;x<count;x++)
-					if (operations[i][j-1]==names[x])           // get the element before the +    x
-						for(int y=0;y<count;y++)
-							if (operations[i][j+1]==names[y])    // get the element after the +    y
-								for (int z=0; z<count;z++)
-								try{
-									if (operations[i][0]==names[z])      // get the element before =   z
-									{
-										Mnames[z]=Mnames[x]+Mnames[y];       // add x and y then store result in z
-										cout<<operations[i][0]<<"="<<endl;
-										//Mnames[z].PrintMatrix();
-										cout<<Mnames[z].getString();
-									}
-								}
-								catch(const char* error){cout<<endl<<"Error : "<<error<<endl;}
-
-
-			}
-			else if (operations[i][j]=='-')                     ///// same for all operations
-			{
-				for(int x=0;x<count;x++)
-					if (operations[i][j-1]==names[x])
-						for(int y=0;y<count;y++)
-							if (operations[i][j+1]==names[y])
-								for (int z=0; z<count;z++)
-								try{
-									if (operations[i][0]==names[z])
-									{
-										Mnames[z]=Mnames[x]-Mnames[y];
-										cout<<operations[i][0]<<"="<<endl;
-										//Mnames[z].PrintMatrix();
-										cout<<Mnames[z].getString();
-									}
-								}
-								catch(const char* error){cout<<endl<<"Error : "<<error<<endl;}
-			}
-			else if (operations[i][j]=='*')
-			{
-				for(int x=0;x<count;x++)
-					if (operations[i][j-1]==names[x])
-						for(int y=0;y<count;y++)
-							if (operations[i][j+1]==names[y])
-								for (int z=0; z<count;z++)
-								try{
-									if (operations[i][0]==names[z])
-									{
-										Mnames[z]=Mnames[x]*Mnames[y];
-										cout<<operations[i][0]<<"="<<endl;
-										cout<<Mnames[z].getString();
-										//Mnames[z].PrintMatrix();
-									}
-								}
-								catch(const char* error){cout<<endl<<"Error : "<<error<<endl;}
-			}
-			else if (operations[i][j]=='/')                                //put this else and the code inside it in a comment and the rest of the code runs correctly
-			{
-				for(int x=0;x<count;x++)
-					if (operations[i][j-1]==names[x])
-						for(int y=0;y<count;y++)
-							if (operations[i][j+1]==names[y])
-								for (int z=0; z<count;z++)
-								try{
-									if (operations[i][0]==names[z])
-									{
-										//cout<<Mnames[y].getDeterminant()<<endl;
-										//if(Mnames[y].getDeterminant()==0)
-										//{
-										//	cout<<"can't devide . Determinant is zero"<<endl;
-										//break;}
-										//else{
-										Mnames[z]=Mnames[x]/Mnames[y];     // doesn't work in big example and code stops here,why? (Dimensions are correct)
-										cout<<operations[i][0]<<"="<<endl;           
-										cout<<Mnames[z].getString();
-										//Mnames[z].PrintMatrix();
-										//}
-									}
-								}
-								catch(const char* w){cout<<endl<<"Error : "<<w<<endl;}
-
-							
-			}
-			else if (operations[i][j]=='.')
-			{
-				
-						for(int y=0;y<count;y++)
-							if (operations[i][j+2]==names[y])
-								for (int z=0; z<count;z++)
-								//try{
-									if (operations[i][0]==names[z])
-									{
-										Mnames[z]=Mnames[y].elediv();
-										cout<<operations[i][0]<<"="<<endl;
-										//Mnames[z].PrintMatrix();
-										cout<<Mnames[z].getString();
-									}
-								//}
-								//catch(const char* error){cout<<endl<<"Error : "<<error<<endl;}
-			}
-			else if (operations[i][j]=='\'')
-			{
-				for(int x=0;x<count;x++)
-					if (operations[i][j-1]==names[x])
-						
-								for (int z=0; z<count;z++)
-								//try{
-									if (operations[i][0]==names[z])
-									{
-										Mnames[z]=Mnames[x].transpose();
-										cout<<operations[i][0]<<"="<<endl;
-										cout<<Mnames[z].getString();
-										//Mnames[z].PrintMatrix();
-									}
-								//}
-								//catch(const char* error){cout<<endl<<"Error : "<<error<<endl;}
-			}
-
-		}
-
-		cout<<endl;
-	}
-
-//}
-//catch(const char* w)
-//{	cout<<endl<<endl<<s<<endl;}
-
-	infile.close();
-}
-else
-{cout<<"The file path not added"<<endl;}
-
-        return 0;
-	}
-	
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-int main(){
-	map < char , CMatrix >  my_map;
-
-
-	my_map.insert( pair < char , CMatrix > ('A',CMatrix("[2.3 2.7;7.6 4.5]")));
-	my_map.insert( pair < char , CMatrix > ('B',CMatrix("[2.3 2.7;7.6 4.5;1.3 22.4]")));
-	my_map.insert( pair < char , CMatrix > ('C',CMatrix("[2.53 2.37;73.6 5.5]")));
-
-
-	//cout<<my_map.size()<<endl;
-try{
-	cout<<(my_map['A']+my_map['B']).getString()<<endl;
-}
-catch(const char* error)
-{cout<<"error : "<<error<<endl;}
-	//map<char,CMatrix>::iterator it =my_map.begin();
-	//cout<<(it->first)<<endl<<((it->second).getString())<<endl;
-
-
-	//string s="[2.0 4.0;6.0 1.0]";
-	//CMatrix x=CMatrix(s);
-	//cout<<x.getString()<<endl;
-	
-
-
-
-	return 0;
-}
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*int x,v=0,index=0;string y;
-			string newinstructions;
-			while(input.find(']',index)!=-1)
-			{v++;
-
-			 x = input.find(']',index);
-		   y=input.substr(index,x+1-index);
-			somefunction(y);
-				index+=(x+1);
-				if(v==2)
-				newinstructions=input.substr(index,input.length()-1-index);
-
-			}
-			
-			
-			
-			cout<<newinstructions<<endl;
-
-
-
-
-
-		*/
